081a90caf8dfce1260bc3157a6177bfa52000056
beast88 <progers999@hotmail.co.uk>
2021-08-08 18:24:40 +0100

    fetch request added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [array, setArray] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151').then((response) =>
+      response.json().then((data) => {
+        setArray(data.results);
+      })
+    );
+  }, []);
 
   return (
     <div>
