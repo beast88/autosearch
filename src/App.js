530e5abdd7828838a8b9220292ccebf79e618db1
beast88 <progers999@hotmail.co.uk>
2021-08-12 12:15:50 +0100

    Search functionality added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 const App = () => {
   const [array, setArray] = useState([]);
   const [search, setSearch] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=151/').then((response) =>
@@ -13,13 +14,31 @@ const App = () => {
     );
   }, []);
 
+  const handleSearch = () => {
+    const newFilter = array.filter((value) =>
+      value.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (search === '') {
+      setSuggestions([]);
+    } else {
+      setSuggestions(newFilter);
+    }
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [search]);
+
   return (
     <div className='container'>
       <input
         className='search'
         type='text'
         value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
       />
     </div>
   );
